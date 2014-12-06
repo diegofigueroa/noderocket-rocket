5577e7e4e7def3edd9b56495c142fef0e042f3ad
Diego Figueroa <diego.figueroa@xoom.com>
2014-12-06 15:42:45 -0500

    updates package.json and height-strategy

--- height-strategy.js
+++ height-strategy.js
@@ -1,8 +1,20 @@
 module.exports = function (rocket) {
   console.log('Using height based strategy');
   var previousAltitude = 0;
+  var lessHeightReadings = 0;
+  var lessHightReadingsTreshold = 3;
   rocket.on('data', function() {
-      console.log("rocket data", data);
-      previousAltitude = data.altitude;
+    console.log("Rocket data", data);
+
+    if (!data.deployed && previousAltitude > 0 && previousAltitude < data.altitude) {
+      lessHeightReadings++;
+      console.log("Rocket altitude decreased to", data.altitude);
+    }
+
+    if (lessHeightReadings === lessHightReadingsTreshold) {
+      console.log("Deploying parachute at", data.altitude);
+      rocket.deployParachute();
+    }
+    previousAltitude = data.altitude;
   });
 };
